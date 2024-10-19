544d79fe98954ab88f19fbea4502181960b47d94
Tan Pham <trongtan260282@gmail.com>
2024-10-19 14:51:33 +0700

    Load

--- DX/obj/Debug/net8.0-windows/DX.AssemblyInfo.cs
+++ DX/obj/Debug/net8.0-windows/DX.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DX")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c46fbafb1a09058ae2cabc15a00f18f9f918e9b5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9e29f0f4b1ce96220d41e8d6c11fc0d0baeb4162")]
 [assembly: System.Reflection.AssemblyProductAttribute("DX")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DX")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
