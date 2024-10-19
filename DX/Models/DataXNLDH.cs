43a0cccafbc577b6d7f02538f6f45469ff3b4280
Bingo <p.tan@mpvuni.com.vn>
2024-10-19 14:08:22 +0700

    24.10.19. Uni

---
+++ DX/Models/DataXNLDH.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DX.Models
+{
+    [Table("XNLDH")]
+    public class DataXNLDH
+    {
+        public int ID { get; set; }
+        [Required]
+        public int Code { get; set; }
+        [Required]
+        [StringLength(30)]
+        public string TenNL { get; set; }
+        [Required]
+        public int Soluongxuat { get; set; }
+        [Required]
+        public DateTime Ngaygioxuat { get; set; }
+        [Required]
+        public int KHThangNam { get; set; }
+        [Required]
+        [StringLength(30)]
+        public string Index { get; set; }
+        [Required]
+        public DateTime Ngaydung { get; set; }
+
+    }
+}
---
+++ DX/ViewModel/DataSPVM.cs
@@ -0,0 +1,28 @@
+﻿using DX.Commands;
+using DX.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace DX.ViewModel
+{
+    public class DataSPVM : INotifyPropertyChanged
+    {
+        private readonly DXSP dbContext;
+        public ObservableCollection<DataSP> DataSPs { get; private set; }
+        public ICommand ImportDataCommand { get;set; }
+
+        public DataSPVM()
+        {
+            dbContext = new DXSP();
+
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+    }
+}
